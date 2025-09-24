6fbd426c67a9739341a4f3a697de7f70e91aab77
Shardul Badoni <140136649+Shardul003@users.noreply.github.com>
2025-09-24 09:41:08 +0530

    Delete pandas_1.0.py

--- pandas_1.0.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-ds=pd.Series([11,12,13,14])
-print(ds)
-data=np.array([[11,12,13,14],[15,16,17,18],[19,20,21,22]])
-print(data)
-df=pd.DataFrame(data)
-print(df)
