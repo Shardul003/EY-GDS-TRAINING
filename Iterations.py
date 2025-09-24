f7245b2088bb71cfb1ea4f018b5c1f5de478d59c
Shardul Badoni <140136649+Shardul003@users.noreply.github.com>
2025-09-24 09:44:17 +0530

    Delete Iterations.py

--- Iterations.py
+++
@@ -1,7 +0,0 @@
-def multiplications(num):
-    print (f" Multiplication of {num})"
-    for i in range (1,11):
-    print (f"{num} X {i}= {num*i})"
-n=int(input("ENTER A NUMBER"))
-obj=multiplications(n)
-print(obj)
