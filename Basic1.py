b81026970dccc777f0e1c8f0a6a8f7d7e737038d
Shardul Badoni <140136649+Shardul003@users.noreply.github.com>
2025-09-24 09:41:56 +0530

    Delete Basic1.py

--- Basic1.py
+++
@@ -1,8 +0,0 @@
-def check_number(num):
-    if num%2==0:
-        return f"{num} is even"
-    else:
-        return f"{num} is odd"
-n=int(input("ENTER A NUMBER"))
-check=check_number(n)
-print(check)
