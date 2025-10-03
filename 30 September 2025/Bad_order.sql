28113f72598292f665c597ff6b2cc916b490403d
Shardul Badoni <140136649+Shardul003@users.noreply.github.com>
2025-10-03 17:05:40 +0530

    Create BadOrders table to store order details

---
+++ 30 September 2025/Bad_order.sql
@@ -0,0 +1,12 @@
+CREATE TABLE BadOrders (
+	order_id      INT PRIMARY KEY,
+  order_date    DATE,
+  customer_id   INT,
+  customer_name VARCHAR(50),
+  customer_city VARCHAR(50),
+  products_ids VARCHAR(200),
+  products_name VARCHAR(200),
+  unit_prices VARCHAR(200),
+  quantities VARCHAR(200),
+  order_total DECIMAL(10,2) 
+);
