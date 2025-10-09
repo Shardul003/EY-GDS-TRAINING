a6348b0cebca5197631dfe42c039b92aae5ed096
Shardul Badoni <140136649+Shardul003@users.noreply.github.com>
2025-10-09 10:27:08 +0530

    Add FastAPI app with sync and async tasks

---
+++ 9 October/Daily activity/concurrency_app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import asyncio
+import time
+app=FastAPI()
+
+@app.get("/sync-task")
+def sync_task():
+    time.sleep(10)
+    return {"message": "Sync task completed after 5 seconds"}
+
+@app.get("/async-task")
+async def async_task():
+    await asyncio.sleep(10)
+    return {"message": "ASync task completed after 5 seconds"}
