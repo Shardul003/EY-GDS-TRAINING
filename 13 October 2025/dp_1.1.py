d0e841a8ec231e2c177789f4689e52a19e8f4de4
Shardul Badoni <140136649+Shardul003@users.noreply.github.com>
2025-10-13 17:04:54 +0530

    Add files via upload

---
+++ 13 October 2025/dp_1.1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from datetime import datetime
+def run_pipeline():
+    df=pd.read_csv("sales.csv")
+    df["Total"]=df["Quantity"]*df["Price"]
+    df.to_csv(path_or_buf="daily_report.csv",index=False)
+    print(f"Pipeline completed at {datetime.now()}")
+
+if __name__=="__main__":
+    run_pipeline()
