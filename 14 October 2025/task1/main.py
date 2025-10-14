d88cd5c832a98b98b165c89d3172514ef7e20f8b
Shardul Badoni <140136649+Shardul003@users.noreply.github.com>
2025-10-14 12:02:36 +0530

    Update main.py

--- 14 October 2025/task1/main.py
+++ 14 October 2025/task1/main.py
@@ -5,17 +5,13 @@ from consumer import consumer
 
 def main():
     q = queue.Queue()
-
     producer_thread = threading.Thread(target=producer, args=(q,))
     consumer_thread = threading.Thread(target=consumer, args=(q,))
-
     producer_thread.start()
     consumer_thread.start()
-
     producer_thread.join()
     consumer_thread.join()
-
-    print("✅ All tasks completed.")
+    print("All tasks completed.")
 
 if __name__ == "__main__":
     main()
