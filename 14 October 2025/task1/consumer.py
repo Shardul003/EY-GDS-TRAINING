8243e074f5e9f4779cfe584d99f158777906ec41
Shardul Badoni <140136649+Shardul003@users.noreply.github.com>
2025-10-14 12:00:15 +0530

    Implement consumer function for queue processing

--- 14 October 2025/task1/consumer.py
+++ 14 October 2025/task1/consumer.py
@@ -1,1 +1,13 @@
+import time
+import random
 
+def consumer(q):
+    """Consumes items from the queue."""
+    while True:
+        item = q.get()
+        if item is None:
+            print("[Consumer] No more items. Exiting.")
+            break
+        print(f"[Consumer] Consuming {item}")
+        time.sleep(random.uniform(1, 2))
+    print("[Consumer] Finished consuming.")
